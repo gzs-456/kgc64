f535cf4562b5510eca7cefa0c4f5b637c96025d2
gzs <2636644031@qq.com>
2020-08-05 21:36:39 +0800

    update gzs

--- springcloud-sport/my-eureka/src/main/java/com/kgc/EurekaServerApplication.java
+++ springcloud-sport/my-eureka/src/main/java/com/kgc/EurekaServerApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class EurekaServerApplication {
     public static void main(String[] args) {
+        //顾忠帅修改
         SpringApplication.run(EurekaServerApplication.class, args);
     }
 }

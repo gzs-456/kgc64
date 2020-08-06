f3e297aacde25ff846167478a82dd88f15cf6dad
yumeiren <381819979@qq.com>
2020-08-06 11:37:59 +0800

    zwl update

---
+++ springcloud-sport/sport-common/src/main/java/com/kgc/pojo/Tb_article.java
@@ -0,0 +1,148 @@
+package com.kgc.pojo;
+
+public class Tb_article {
+    private String id;
+    private String columnid;
+    private String userid;
+    private String title;
+    private String content;
+    private String image;
+    private String createtime;
+    private String updatetime;
+    private String ispublic;
+    private String istop;
+    private Integer visits;
+    private Integer thumbup;
+    private Integer commrnt;
+    private String state;
+    private Integer channelid;
+    private String url;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getColumnid() {
+        return columnid;
+    }
+
+    public void setColumnid(String columnid) {
+        this.columnid = columnid;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(String createtime) {
+        this.createtime = createtime;
+    }
+
+    public String getUpdatetime() {
+        return updatetime;
+    }
+
+    public void setUpdatetime(String updatetime) {
+        this.updatetime = updatetime;
+    }
+
+    public String getIspublic() {
+        return ispublic;
+    }
+
+    public void setIspublic(String ispublic) {
+        this.ispublic = ispublic;
+    }
+
+    public String getIstop() {
+        return istop;
+    }
+
+    public void setIstop(String istop) {
+        this.istop = istop;
+    }
+
+    public Integer getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Integer visits) {
+        this.visits = visits;
+    }
+
+    public Integer getThumbup() {
+        return thumbup;
+    }
+
+    public void setThumbup(Integer thumbup) {
+        this.thumbup = thumbup;
+    }
+
+    public Integer getCommrnt() {
+        return commrnt;
+    }
+
+    public void setCommrnt(Integer commrnt) {
+        this.commrnt = commrnt;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Integer getChannelid() {
+        return channelid;
+    }
+
+    public void setChannelid(Integer channelid) {
+        this.channelid = channelid;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}

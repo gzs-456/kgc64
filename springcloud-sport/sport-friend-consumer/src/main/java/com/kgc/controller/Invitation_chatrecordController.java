f3a059e09504588560f727d4af96901847f7dc3b
gzs <2636644031@qq.com>
2020-08-24 14:49:53 +0800

    gzs redis

--- springcloud-sport/sport-friend-consumer/src/main/java/com/kgc/controller/Invitation_chatrecordController.java
+++ springcloud-sport/sport-friend-consumer/src/main/java/com/kgc/controller/Invitation_chatrecordController.java
@@ -1,8 +1,10 @@
 package com.kgc.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.kgc.pojo.Invitation_chatrecord;
 import com.kgc.service.Invitation_chatrecordService;
 import com.kgc.util.PageUtil;
+import com.kgc.util.RedisUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +15,8 @@ import java.util.Map;
 public class Invitation_chatrecordController {
     @Autowired
     private Invitation_chatrecordService chatrecordService;
+    @Autowired
+    private RedisUtils redisUtils;
     //分页，根据发送方和接收方编号查询所有聊天信息,并按照发送时间降序排列
     @RequestMapping("/getAllInvitation_chatrecord")
         public PageUtil<Invitation_chatrecord> getAllInvitation_chatrecord(String putid,String gainid,
